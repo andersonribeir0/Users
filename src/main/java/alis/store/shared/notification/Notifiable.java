2955a681df38eeeacdc9dea2df193fa22fc75e2d
Anderson <anderson.moouro@gmail.com>
2018-03-03 13:46:00 -0300

    implementing notification pattern

--- src/main/java/alis/store/shared/notification/Notifiable.java
+++ src/main/java/alis/store/shared/notification/Notifiable.java
@@ -1,15 +1,32 @@
 package alis.store.shared.notification;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public abstract class Notifiable {
-    private List<Notification> Notification;
     
-    public boolean isValid(){
-        return Notification.isEmpty();
+    private List<Notification> Notifications;
+    private boolean isValid = isValid();
+    
+    protected Notifiable(){
+        Notifications = new ArrayList<>();
     }
     
-    public void AddNotification(String object, String message){
+    private boolean isValid(){
+        return Notifications.isEmpty();
+    }
         
+    public void AddNotification(String attribute, String message){
+        Notifications.add(new Notification(attribute,message));
     }
+    
+    public void AddNotifications(List<Notification> notifications){
+        Notifications.addAll(notifications);
+    }
+    
+    public void AddNotifications(Notifiable item){
+        AddNotifications(item.Notifications);
+    }
+    
 }
--- src/main/java/alis/store/shared/notification/Notification.java
+++ src/main/java/alis/store/shared/notification/Notification.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package alis.store.shared.notification;
 
-/**
- *
- * @author andersonribeiro
- */
 public class Notification {
+    
+    private String Attribute;
+    private String Message;
+    
+    public Notification(String attribute, String message){
+        this.Attribute = attribute;
+        this.Message = message;
+    }
+
+    public String getAttribute() {
+        return Attribute;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+    
     
 }
