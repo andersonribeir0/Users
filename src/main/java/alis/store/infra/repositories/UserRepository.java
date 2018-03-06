d9100cc73799ea6c63c974bc0fc3bcf9168d672b
Anderson Ribeiro <anderson.moouro@gmail.com>
2018-03-06 16:07:04 -0300

    fix getAll method

--- src/main/java/alis/store/infra/repositories/UserRepository.java
+++ src/main/java/alis/store/infra/repositories/UserRepository.java
@@ -1,14 +1,20 @@
 package alis.store.infra.repositories;
 
 import alis.store.domain.entities.User;
+import alis.store.domain.queries.QueryAllUsersResult;
 import alis.store.domain.repositories.IUserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserRepository implements IUserRepository{
     private List<User> users;
-
+    
+    @Autowired
     public UserRepository() {
         this.users = new ArrayList<>();
     }
@@ -30,11 +36,20 @@ public class UserRepository implements IUserRepository{
     }
 
     public void AddUser(User user) {
-        users.add(user);
+     	users.add(user);
     }
 
-    public List<User> GetAll(){
-        return users;
+    public List<QueryAllUsersResult> GetAll(){
+        List<QueryAllUsersResult> allUsers = new ArrayList<>();
+        for (User user : users){
+            QueryAllUsersResult qUser = new QueryAllUsersResult();
+            qUser.Address = user.getAddress().toString();
+            qUser.Document = user.getDocument().toString();
+            qUser.Email = user.getEmail().toString();
+            qUser.Name = user.getName().toString();
+            allUsers.add(qUser);
+        }
+        return allUsers;
     }
 
 }
